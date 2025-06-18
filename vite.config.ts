1715409cc2e266f16b12ae05957799f1af563599
monics-hub <148586659+monics-hub@users.noreply.github.com>
2025-06-18 13:36:28 -0600

    Test #1

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-calculator/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/react-calculator/',
 })
